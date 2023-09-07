d2900be9fb662dacbd41d9a33971d37008567256
jihooDev <wlgn829@kakao.com>
2023-09-07 10:35:05 +0900

    test

--- page/Home.tsx
+++ page/Home.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import CustomSafeAreaView from '../component/CustomSafeAreaView'
-import { Text } from 'react-native'
 import { COLORS } from '../asset/asset'
-import CustomText from '../component/CustomText'
-import { MotiView } from 'moti'
 
 const Home = () => {
     return (
         <CustomSafeAreaView backColor={COLORS.white}>
+            <></>
         </CustomSafeAreaView>
     )
 }
